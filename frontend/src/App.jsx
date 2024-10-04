2a8bdb44e804c04dac281f52df204be1103cc67f
Lysenko <lysenkoden@gmail.com>
2024-10-04 23:11:44 +0200

    started creation the CreatePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import { Button } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import Navbar from '../src/components/NavBar';
+import { Box } from '@chakra-ui/react';
 
 function App() {
   return (
-    <>
-      <Button>Hello</Button>
-    </>
+    <Box minH={'100vh'}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="create" element={<CreatePage />} />
+      </Routes>
+    </Box>
   );
 }
 
