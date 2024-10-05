e61edd8d5eb50139f0ef40b061641587fbca3e20
Lysenko <lysenkoden@gmail.com>
2024-10-05 09:08:03 +0200

    added UI to create product

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,18 @@
-import { Container, Flex, Text } from '@chakra-ui/react';
+import {
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Text,
+  useColorMode,
+} from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import { CiCirclePlus, CiLight } from 'react-icons/ci';
+import { MdLightMode } from 'react-icons/md';
 
 const NavBar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Container maxW={'1140px'} px={4}>
       <Flex
@@ -21,8 +32,22 @@ const NavBar = () => {
           bgGradient={'linear(to-r, cyan.400, blue.500)'}
           bgClip={'text'}
         >
-          <Link to={'/'}>Product Store</Link>
+          <Link to={'/'}>Product Store🛒</Link>
         </Text>
+        <HStack spacing={2} alignItems={'center'}>
+          <Link to="/create">
+            <Button>
+              <CiCirclePlus size="20" />
+            </Button>
+            <Button onClick={toggleColorMode}>
+              {colorMode === 'light' ? (
+                <CiLight size="20" />
+              ) : (
+                <MdLightMode size="20" />
+              )}
+            </Button>
+          </Link>
+        </HStack>
       </Flex>
     </Container>
   );
